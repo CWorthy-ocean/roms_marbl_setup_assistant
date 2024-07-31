84a28ceafeb110a0583103ed1f87c715103775bc
“Dafydd <dafydd_stephenson@hotmail.com>
2024-07-31 16:15:32 -0600

    overhaul repository: remove use of manage_externals/manic, move to a single script for setup, remove references to C-Star

---
+++ setup_files/externals_info.sh
@@ -0,0 +1,10 @@
+# External codebase configurations for bash sourcing
+# ROMS
+ROMS_REPO_URL="https://github.com/CESR-lab/ucla-roms.git"
+ROMS_LOCAL_PATH="ucla-roms"
+ROMS_CHECKOUT_TARGET="e7d0e9f38271cea22cbf253e4aa4c1187e242c25" # commit hash
+
+# MARBL
+MARBL_REPO_URL="https://github.com/marbl-ecosys/MARBL.git"
+MARBL_LOCAL_PATH="MARBL"
+MARBL_CHECKOUT_TARGET="marbl0.45.0" # tag
