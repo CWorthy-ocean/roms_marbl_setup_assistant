c210eb40f4148dd4b34b67e338447b7af792b526
Dafydd Stephenson <dafydd@derecho2.hsn.de.hpc.ucar.edu>
2024-08-15 12:21:34 -0600

    improve prompts; expand system check to include LMOD_SYSTEM_NAME is LMOD_SYSHOST is undefined; change roms install to just checkout main

--- setup_files/externals_info.sh
+++ setup_files/externals_info.sh
@@ -2,7 +2,7 @@
 # ROMS
 ROMS_REPO_URL="https://github.com/CESR-lab/ucla-roms.git"
 ROMS_LOCAL_PATH="ucla-roms"
-ROMS_CHECKOUT_TARGET="2a4e9107ffc771d2833fbea3e08f1e169759b5ff" # commit hash
+ROMS_CHECKOUT_TARGET="main" # branch
 
 # MARBL
 MARBL_REPO_URL="https://github.com/marbl-ecosys/MARBL.git"
